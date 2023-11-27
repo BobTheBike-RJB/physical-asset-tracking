60f38377647538cb5b1d58208dfe0b5d974363e2
BobTheBike <rbabichjr@gmail.com>
2023-11-27 22:14:48 +0100

    Adding logic for requests to the "code" path

--- routes/code.js
+++ routes/code.js
@@ -9,7 +9,7 @@ const router = express.Router()
 //   next()
 // })
 
-// define the home page route
+//When the '/code' path is requested generally
 router.get('/', (req, res) => {
   res.send(`'codes' home page`)
 })
@@ -17,6 +17,11 @@ router.get('/', (req, res) => {
 //When a specific code is requested
 router.get('/:id', (req, res) => {
   res.send("The requested code: " + req.params.id)
+})
+
+//When the '/code' path is posted, creates a new code linked to the authorized user
+router.post('/', (req, res) => {
+  res.send(`This should create a new code, not yet configured.`)
 })
 
 module.exports = router
